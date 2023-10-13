a12cebd089b0167be07fd01bec855dba7b69b4fb
Amber <amber@storyboard.fm>
2023-10-13 11:29:18 -0400

    Add vellex

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ data['ID'] = [str(i) for i in data['ID']]
 
 
 # Sidebar
-pages = ["Overview"] + list(data["Service"].unique()) + ["Other"]
+pages = ["Overview"] + list(data["Service"].unique()) + ["ValleX"] + ["Other"]
 selected_page = st.sidebar.selectbox("Select a Page", pages)
-services = ['Amazon Polly*', 'Eleven Labs*', 'Google Cloud TTS*', 'PlayHT*', 'Coqui*', 'Wellsaid', 'Bark', 'Speechify', 'Facebook/mms-tts']
+services = ['Amazon Polly*', 'Eleven Labs*', 'Google Cloud TTS*', 'PlayHT*', 'Coqui*', 'ValleX*', 'Wellsaid', 'Bark', 'Speechify', 'Facebook/mms-tts']
 
 def render_overview_page():
     st.title("Overview")
@@ -63,56 +63,86 @@ def render_service_page(service_selected):
     filtered_data = data[data["Service"] == service_selected]
 
     languages = filtered_data["Language"].unique()
+    if service_selected == "ValleX":
+        st.write("[Storyboard Valle-X Clone on Hugging Face](https://huggingface.co/spaces/storyboard-fm/VALL-E-X)")
+        st.write("[Valle-X on Hugging Face](https://huggingface.co/spaces/Plachta/VALL-E-X)")
 
-    st.write("## Stats")
+        st.write('### Audio Samples')
+        # Dropdown for voice selection
+        st.write('These samples are all created using 2 different audio prompts of JP. Prompt is generated with less than 15 seconds of audio.')
 
-    # Display statistics for the selected service
-    if service_selected == "Coqui":
-        st.write(":red[Does not support CN and cannot handle multiligual synthesis. + 250 Character Limit (async)]")
-        st.write("[Pricing](https://coqui.ai/pricing)")
-    elif service_selected == "PlayHT":
-        st.write(":red[Only supports english voices as default (can add custom voices) + 200 Character Limit (async)]")
-        st.write("[Pricing](https://play.ht/pricing/)")
-    elif service_selected == "Eleven Labs":
-        st.write("[Pricing](https://elevenlabs.io/pricing)")
-    elif service_selected == "Google Cloud TTS":
-        st.write("[Pricing](https://cloud.google.com/text-to-speech/pricing)")
-    elif service_selected == "Amazon Polly":
-        st.write("[Pricing](https://aws.amazon.com/polly/pricing/)")
 
-    st.write("### Latency")
-    # Average latency for the entire service
-    avg_latency = filtered_data["Latency (s)"].mean()
-    st.write(f"Average Latency for {service_selected} OVERALL: {avg_latency:.2f} seconds")
-    latency_per_lang = filtered_data.groupby("Language")["Latency (s)"].mean()
-    for lang, latency in latency_per_lang.items():
-        st.write(f"{lang}: {latency:.2f} seconds")
-    st.write('### Data')
-    st.write(filtered_data)
+        st.write("### Audio Prompt = JP1")
+        st.write("##### preset=acou_2")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_OG_prompt/acou2.wav", format='audio/wav')
+        st.write("#####  preset=neutral")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_OG_prompt/neutral.wav", format='audio/wav')
+        st.write("#####  preset=Angry")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_OG_prompt/angry.wav", format='audio/wav')
+        st.write("##### Long Text")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_OG_prompt/long_text.wav", format='audio/wav')
+       
+        st.write("### Audio Prompt = JP2")
+        st.write("##### preset=acou_2")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_JP_2/acou_2.wav", format='audio/wav')
+        st.write("#####  preset=neutral")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_JP_2/neutral.wav", format='audio/wav')
+        st.write("#####  preset=Angry")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_JP_2/angry.wav", format='audio/wav')
+        st.write("##### Long Text")
+        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/ValleX/english_JP_2/long_text.wav", format='audio/wav')
+       
 
-    st.write('### Audio Samples')
-    # Dropdown for voice selection
-    voice_selected = st.selectbox("Select a Voice", filtered_data["Voice"].unique())
-    filtered_data_voice = filtered_data[filtered_data["Voice"] == voice_selected]
+    else: 
+        st.write("## Stats")
+        # Display statistics for the selected service
+        if service_selected == "Coqui":
+            st.write(":red[Does not support CN and cannot handle multiligual synthesis. + 250 Character Limit (async)]")
+            st.write("[Pricing](https://coqui.ai/pricing)")
+        elif service_selected == "PlayHT":
+            st.write(":red[Only supports english voices as default (can add custom voices) + 200 Character Limit (async)]")
+            st.write("[Pricing](https://play.ht/pricing/)")
+        elif service_selected == "Eleven Labs":
+            st.write("[Pricing](https://elevenlabs.io/pricing)")
+        elif service_selected == "Google Cloud TTS":
+            st.write("[Pricing](https://cloud.google.com/text-to-speech/pricing)")
+        elif service_selected == "Amazon Polly":
+            st.write("[Pricing](https://aws.amazon.com/polly/pricing/)")
 
-    languages = filtered_data_voice["Language"].unique()
-    for lang in languages:
-        st.write(f"### Language: {lang}")
-        lang_data = filtered_data_voice[filtered_data_voice["Language"] == lang]
-        
-        for index, row in lang_data.iterrows():
-            row_display = f"{row['Text']}; latency: {row['Latency (s)']} seconds"
-            clicked = st.button(row_display, key=f"{row['ID']}-{index}")
+        st.write("### Latency")
+        # Average latency for the entire service
+        avg_latency = filtered_data["Latency (s)"].mean()
+        st.write(f"Average Latency for {service_selected} OVERALL: {avg_latency:.2f} seconds")
+        latency_per_lang = filtered_data.groupby("Language")["Latency (s)"].mean()
+        for lang, latency in latency_per_lang.items():
+            st.write(f"{lang}: {latency:.2f} seconds")
+        st.write('### Data')
+        st.write(filtered_data)
+
+        st.write('### Audio Samples')
+        # Dropdown for voice selection
+        voice_selected = st.selectbox("Select a Voice", filtered_data["Voice"].unique())
+        filtered_data_voice = filtered_data[filtered_data["Voice"] == voice_selected]
+
+        languages = filtered_data_voice["Language"].unique()
+        for lang in languages:
+            st.write(f"### Language: {lang}")
+            lang_data = filtered_data_voice[filtered_data_voice["Language"] == lang]
             
-            if clicked:
-                if service_selected == "Coqui":
-                    st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/Coqui/{row['ID']}.wav", format='audio/wav')
-                elif service_selected == "PlayHT":
-                    st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/PlayHT/{row['ID']}.wav", format='audio/wav')
-                elif service_selected != "Coqui": 
-                    st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/{row['Service'].replace(' ', '+')}/{row['ID']}.mp3", format='audio/mp3')
-                else:
-                    st.write("Audio file not found.")
+            for index, row in lang_data.iterrows():
+                row_display = f"{row['Text']}; latency: {row['Latency (s)']} seconds"
+                clicked = st.button(row_display, key=f"{row['ID']}-{index}")
+                
+                if clicked:
+                    if service_selected == "Coqui":
+                        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/Coqui/{row['ID']}.wav", format='audio/wav')
+                    elif service_selected == "PlayHT":
+                        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/PlayHT/{row['ID']}.wav", format='audio/wav')
+                    elif service_selected != "Coqui": 
+                        st.audio(f"https://content-intelligence-output-dev.s3.us-east-2.amazonaws.com/TTS/outputs/audio/{row['Service'].replace(' ', '+')}/{row['ID']}.mp3", format='audio/mp3')
+                    else:
+                        st.write("Audio file not found.")
+
 def render_other_page():
     st.title('Other')
     st.write("# Bark")
